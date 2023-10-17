2a6ede884b3c3dc09cc9dbbd1b5c064bf068e572
aymen2782 <aymen.sellaouti@gmail.com>
2023-10-17 15:12:33 +0100

    products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,14 @@
 import { Component } from "@angular/core";
+import {
+  BehaviorSubject,
+  Observable,
+  Subject,
+  concatMap,
+  map,
+  scan,
+  takeWhile,
+} from "rxjs";
+import { ProductService } from "./service/product.service";
 
 export interface ApiResponse {
   limit: number;
@@ -31,4 +41,32 @@ export interface Settings {
   templateUrl: "./products.component.html",
   styleUrls: ["./products.component.css"],
 })
-export class ProductsComponent {}
+export class ProductsComponent {
+  /*
+    aller chercher les produits paginés
+    à chaque fois qu'on tape sur more =>
+    1- Flux pour le cli🇨 => Subject
+    2- Le http MergeMap X , ConcatMap
+    3- Flux pour les produits (résultat des deux flux ) scan
+  */
+  products$: Observable<Product[]>;
+  settings: Settings = { limit: 12, skip: 0 };
+  private settings$: BehaviorSubject<Settings> = new BehaviorSubject(
+    this.settings
+  );
+  constructor(private productService: ProductService) {
+    this.products$ = this.getProdducts();
+  }
+  private getProdducts(): Observable<Product[]> {
+    return this.settings$.pipe(
+      concatMap((settings) => this.productService.getProducts(settings)),
+      map((apiResponse) => apiResponse.products),
+      takeWhile((produits) => !!produits.length),
+      scan((oldProducts, newProducts) => [...oldProducts, ...newProducts])
+    );
+  }
+  more() {
+    this.settings.skip += 12;
+    this.settings$.next(this.settings);
+  }
+}
