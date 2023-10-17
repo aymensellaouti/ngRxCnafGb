8385e849f8cca83d66a3faab7c3a0e445e998ae2
aymen2782 <aymen.sellaouti@gmail.com>
2023-10-17 11:13:16 +0100

    slider avec combineLatest

--- src/app/rxjs/slider/slider.component.ts
+++ src/app/rxjs/slider/slider.component.ts
@@ -1,6 +1,15 @@
+import { HttpClient } from "@angular/common/http";
 import { Component, Input, OnInit } from "@angular/core";
-import { Observable, timer, map } from "rxjs";
+import { Observable, timer, map, combineLatest, tap } from "rxjs";
+import { API } from "../../../config/api.config";
 
+export interface Photo {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
 @Component({
   selector: "app-slider",
   templateUrl: "./slider.component.html",
@@ -17,13 +26,21 @@ export class SliderComponent implements OnInit {
   ];
   @Input() size = 150;
   @Input() time = 1000;
+  images$: Observable<Photo[]>;
+  constructor(private http: HttpClient) {
+    this.images$ = this.http.get<Photo[]>(API.photos);
+  }
   slider$!: Observable<string>;
   ngOnInit(): void {
-    this.slider$ =
-      /* 0 1 2 3 4 5 6 ... */
-      /* 404 as cv */
-      timer(0, this.time).pipe(
+    this.slider$ = combineLatest([this.images$, timer(0, this.time)]).pipe(
+      tap(([images, index]) => console.log({ images, index })),
+      map(([images, index]) => images[index % (images.length - 1)].url)
+    );
+
+    /* 0 1 2 3 4 5 6 ... */
+    /* 404 as cv */
+    /*       timer(0, this.time).pipe(
         map((index) => this.images[index % (this.images.length - 1)])
-      );
+      ); */
   }
 }
